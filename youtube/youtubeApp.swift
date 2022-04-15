accdd0593226c0cda680bab4288ed49e3616778f
qkrdygks3023 <55884525+qkrdygks3023@users.noreply.github.com>
2022-04-15 15:30:43 +0900

    Initial Commit

---
+++ youtube/youtubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtubeApp.swift
+//  youtube
+//
+//  Created by 박요한 on 2022/04/15.
+//
+
+import SwiftUI
+
+@main
+struct youtubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
